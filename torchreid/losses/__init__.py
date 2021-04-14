3b23ebb1a603cc1adcca41cef9f3967e84dff147
Arindam Sikdar <as26@iitbbs.ac.in>
2021-04-14 16:28:55 +0530

    Update __init__.py

--- torchreid/losses/__init__.py
+++ torchreid/losses/__init__.py
@@ -3,6 +3,8 @@ from __future__ import division, print_function, absolute_import
 from .cross_entropy_loss import CrossEntropyLoss
 from .hard_mine_triplet_loss import TripletLoss
 from .mmd_loss import MMDLoss
+from .class_memory_loss import ClassMemoryLoss
+from .pairwise_matching_loss import PairwiseMatchingLoss
 
 
 def DeepSupervision(criterion, xs, y):
