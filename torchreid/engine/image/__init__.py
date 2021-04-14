5aa8025339290a85ff98bc88211bf3c0dd8b4685
Arindam Sikdar <as26@iitbbs.ac.in>
2021-04-14 16:46:07 +0530

    Update __init__.py

--- torchreid/engine/image/__init__.py
+++ torchreid/engine/image/__init__.py
@@ -3,3 +3,4 @@ from __future__ import absolute_import
 from .softmax import ImageSoftmaxEngine
 from .triplet import ImageTripletEngine
 from .triplet_DG import ImageTripletEngine_DG
+from .classmemoryloss_QA import ImageQAConvEngine
