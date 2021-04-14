010cf4e7ad435557fbcae805e406ccf2d655fdc4
Arindam Sikdar <as26@iitbbs.ac.in>
2021-04-14 16:26:09 +0530

    Update __init__.py

--- torchreid/engine/__init__.py
+++ torchreid/engine/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import print_function, absolute_import
 
-from .image import ImageSoftmaxEngine, ImageTripletEngine, ImageTripletEngine_DG
+from .image import ImageSoftmaxEngine, ImageTripletEngine, ImageTripletEngine_DG, ImageQAConvEngine
 from .video import VideoSoftmaxEngine, VideoTripletEngine
 from .engine import Engine
