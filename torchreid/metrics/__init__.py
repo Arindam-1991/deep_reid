d74a70888883e5c3f1815f9c7ab98faee48d62f4
Arindam Sikdar <as26@iitbbs.ac.in>
2021-04-20 16:57:47 +0530

    Update __init__.py

--- torchreid/metrics/__init__.py
+++ torchreid/metrics/__init__.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import
 
 from .rank import evaluate_rank
 from .accuracy import accuracy
-from .distance import compute_distance_matrix
+from .distance import compute_distance_matrix, pairwise_distance_using_QAmatcher
